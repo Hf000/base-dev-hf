6a0fefe58f77bc57d8bfb6eaadccec63705a9ab9
HF-PC\HF <809969840@qq.com>
2022-08-06 13:13:10 +0800

    springBoot的简单应用修改

--- boot-dev/spring-boot-demo/src/main/java/org/hf/boot/SpringBootDemoApplication.java
+++ boot-dev/spring-boot-demo/src/main/java/org/hf/boot/SpringBootDemoApplication.java
@@ -6,20 +6,13 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * <p> springboot启动类 </p>
+ * //@SpringBootApplication //springboot启动类注解
+ * //@MapperScan("org.hf.**.dao") //开启扫描mybatis所有业务mapper接口的包路径, 整合通用mapper后需要将这个注解注释掉
+ * //@MapperScan("org.hf.**.dao") //整合通用mapper需要注释掉mybatis官方的@MapperScan注解，开启通用mapper的@MapperScan注解
  * @author hufei
  * @date 2022/8/3 22:00
 */
-/**
- * springboot启动类注解
- */
 @SpringBootApplication
-/**
- * 开启扫描mybatis所有业务mapper接口的包路径, 整合通用mapper后需要将这个注解注释掉
- */
-//@MapperScan("org.hf.**.dao")
-/**
- * 整合通用mapper需要注释掉mybatis官方的@MapperScan注解，开启通用mapper的@MapperScan注解
- */
 @MapperScan("org.hf.**.dao")
 public class SpringBootDemoApplication {
     public static void main(String[] args) {
--- boot-dev/spring-boot-demo/src/main/java/org/hf/boot/springboot/dao/UserMapper.java
+++ boot-dev/spring-boot-demo/src/main/java/org/hf/boot/springboot/dao/UserMapper.java
@@ -1,19 +1,26 @@
 package org.hf.boot.springboot.dao;
 
+import org.hf.boot.springboot.mapper.BaseMapper;
 import org.hf.boot.springboot.pojo.entity.User;
 import org.springframework.stereotype.Repository;
-import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 /**
  * @Author:hufei
  * @CreateTime:2020-09-09
- * @Description:用户实体对应mapper
+ * @Description:
  */
-//@Mapper    //如果启动类开启了@MapperScan注解，这里不需要这个注解了
-@Repository //这个注解可加可不加
-public interface UserMapper extends Mapper<User> {//BaseMapper继承通用Mapper<T>和MySqlMapper<T>
+/**
+ * <p> 用户实体对应mapper </p>
+ * //@Mapper    //如果启动类开启了@MapperScan注解，这里不需要这个注解了
+ * 注解@Repository可加可不加 防止idea报红
+ * //BaseMapper继承通用Mapper<T>和MySqlMapper<T> {@link BaseMapper}
+ * @author hufei
+ * @date 2022/8/6 12:28
+*/
+@Repository
+public interface UserMapper extends BaseMapper<User> {
     User getUser(long id);
 
     List<User> getUserAll();
