6d7440093650b6eee1dc4731f3ae55549fe79af0
HF-PC\HF <809969840@qq.com>
2022-08-07 21:53:41 +0800

    修改

--- application-dev/java-base-demo/src/main/java/org/hf/application/javabase/design/patterns/structural/proxy/jdk/JdkProxyTest.java
+++ application-dev/java-base-demo/src/main/java/org/hf/application/javabase/design/patterns/structural/proxy/jdk/JdkProxyTest.java
@@ -4,30 +4,35 @@ import sun.misc.ProxyGenerator;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Proxy;
 
 
 public class JdkProxyTest {
 
-    public static void main(String[] args)throws Exception {
-        //需要被创建代理的对象
-        LandlordService landlordService = new Landlord();
+    public static void main(String[] args) {
+        try {
+            //需要被创建代理的对象
+            LandlordService landlordService = new Landlord();
 
-        //创建代理
-        LandlordService proxyLandlordService = (LandlordService) Proxy.newProxyInstance(
-                landlordService.getClass().getClassLoader(),
-                Landlord.class.getInterfaces(), //被代理的对象实现的所有接口
-                //代理过程->InvocationHandler
-                new QFang(landlordService)
-                );
+            //创建代理
+            LandlordService proxyLandlordService = (LandlordService) Proxy.newProxyInstance(
+                    landlordService.getClass().getClassLoader(),
+                    Landlord.class.getInterfaces(), //被代理的对象实现的所有接口
+                    //代理过程->InvocationHandler
+                    new QFang(landlordService)
+                    );
 
-        proxyLandlordService.pay("王五");
+            proxyLandlordService.pay("王五");
 
-        byte[] proxyClass = ProxyGenerator.generateProxyClass(proxyLandlordService.getClass().getSimpleName(), proxyLandlordService.getClass().getInterfaces());
-        //将字节码文件保存到D盘，文件名为$Proxy0.class
-        FileOutputStream outputStream = new FileOutputStream(new File("d:\\$Proxy0.class"));
-        outputStream.write(proxyClass);
-        outputStream.flush();
-        outputStream.close();
+            byte[] proxyClass = ProxyGenerator.generateProxyClass(proxyLandlordService.getClass().getSimpleName(), proxyLandlordService.getClass().getInterfaces());
+            //将字节码文件保存到D盘，文件名为$Proxy0.class
+            FileOutputStream outputStream = new FileOutputStream(new File("d:\\$Proxy0.class"));
+            outputStream.write(proxyClass);
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
