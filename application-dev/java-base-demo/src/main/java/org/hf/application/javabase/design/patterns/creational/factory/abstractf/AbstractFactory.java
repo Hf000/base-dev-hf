5b85d9c1e7692c5b18e99f754d46fccd0273dfcd
HF-PC\HF <809969840@qq.com>
2022-10-06 13:13:44 +0800

    修改

--- application-dev/java-base-demo/src/main/java/org/hf/application/javabase/design/patterns/creational/factory/abstractf/AbstractFactory.java
+++ application-dev/java-base-demo/src/main/java/org/hf/application/javabase/design/patterns/creational/factory/abstractf/AbstractFactory.java
@@ -3,8 +3,9 @@ package org.hf.application.javabase.design.patterns.creational.factory.abstractf
 /**
  * <p> 工厂模式-抽象工厂 </p>
  * 和工厂方法的区别:
- *      抽象工厂: 创建的是一类的对象, 可以理解为一个品牌里面的不同产品
- *      工厂方法: 创建的是某一个对象, 没有区分一类的概念
+ * 抽象工厂: 创建的是一类的对象, 可以理解为一个品牌里面的不同产品
+ * 工厂方法: 创建的是某一个对象, 没有区分一类的概念
+ *
  * @author hufei
  * @version 1.0.0
  * @date 2022/4/23 15:38
