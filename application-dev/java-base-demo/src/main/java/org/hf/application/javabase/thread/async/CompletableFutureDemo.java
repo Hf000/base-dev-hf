2d4561485429bbbcb65f665e484b88f2dd9a2d73
HF-PC\HF <809969840@qq.com>
2025-05-08 13:44:17 +0800

    CompletableFutureUtil工具使用避坑

--- application-dev/java-base-demo/src/main/java/org/hf/application/javabase/thread/async/CompletableFutureDemo.java
+++ application-dev/java-base-demo/src/main/java/org/hf/application/javabase/thread/async/CompletableFutureDemo.java
@@ -22,7 +22,11 @@ public class CompletableFutureDemo {
             CompletableFuture<Void> future = CompletableFuture.runAsync(() -> test(finalI), executorService);
             taskList.add(future);
         }
-        // 注意如果使用此方式, 需要注意线程池的队列长度问题, 如果提交的任务数最后超过了队列能承载的总大小, 会导致此方法获取结果一直阻塞等待
+        /**
+         * 注意如果使用此方式, 需要注意线程池的队列长度问题, 如果提交的任务数最后超过了队列能承载的总大小,会导致此方法获取结果一直阻塞等待,
+         * 原因:如果提交的任务超出线程池处理的数量,那么超出的任务将会被线程池执行拒接策略,这样会导致CompletableFuture无法将线程池抛弃的
+         * 任务标记为已完成,而导致调用allOf方法一直处于阻塞状态
+         */
         CompletableFutureUtil.blockWaitingUntilFinished(taskList);
         System.out.println("执行完成");
         ThreadPoolUtil.closeJdkThreadPool(executorService);
--- application-dev/java-base-demo/src/main/java/org/hf/application/javabase/utils/CompletableFutureUtil.java
+++ application-dev/java-base-demo/src/main/java/org/hf/application/javabase/utils/CompletableFutureUtil.java
@@ -17,7 +17,11 @@ public class CompletableFutureUtil {
      */
     public static void blockWaitingUntilFinished(List<CompletableFuture<Void>> futures) {
         try {
-            // 等待所有线程执行完成, 这里其实不用强制捕获异常, 这里是为了防止业务异常
+            /**
+             * 等待所有线程执行完成, 这里其实不用强制捕获异常, 这里是为了防止业务异常, 注意:如果提交的任务超出线程池处理的数量,
+             * 那么超出的任务将会被线程池执行拒接策略,这样会导致CompletableFuture无法将线程池抛弃的任务标记为已完成,而导致调用
+             * allOf方法一直处于阻塞状态
+             **/
             CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         }  catch (Exception e) {
             throw new RuntimeException(e);
